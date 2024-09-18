989d51c1c4c449628162d601101e4050c7ef60c1
ShreyP. <shreyprajapati13@gmail.com>
2024-09-18 17:41:01 +0530

    delete shit

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-   base: "./",
-  // base: "https://github.com/cd-Shrey13/Flood-Prediction-Tool/"
-})
