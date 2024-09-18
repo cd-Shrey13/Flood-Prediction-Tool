ff2393a287a6596775a132e8dbaab1b67e7f5e7d
Shrey Prajapati <shreyprajapati13@gmail.com>
2024-09-18 16:35:18 +0530

    update base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://cd-shrey13.github.io/Flood-Prediction-Tool/"
+  base: "https://github.com/cd-Shrey13/Flood-Prediction-Tool.git"
 })
