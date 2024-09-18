5a628f4547a5e652c2896c9a2878712cffc919c7
ShreyP. <shreyprajapati13@gmail.com>
2024-09-18 17:21:40 +0530

    remove map

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState, useEffect } from "react";
-import Map from "./components/Map";
+// import Map from "./components/Map";
 import SafeZones from './components/SafeZones'
 import Footer from './components/Footer'
 
@@ -42,7 +42,7 @@ function App() {
     <section className="app size-8 w-full">
      
       <div className="grid  grid-cols-2 grid-rows-2 gap-4 p-4 h-[80vh]">
-        <Map className="col-start-1 col-end-2 row-start-1 row-end-3 rounded-xl h-full w-full bg-amber-500" />
+        {/* <Map className="col-start-1 col-end-2 row-start-1 row-end-3 rounded-xl h-full w-full bg-amber-500" /> */}
         <SafeZones
           title="Alerts"
           className="bg-gray-200 h-full w-full col-start-2 row-start-1 col-end-3 row-end-2 rounded-xl prediction border-2 border-gray-100 p-4 flex flex-col gap-4"
